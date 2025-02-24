381c1e4a4be9f5155627bdddce91388cfe0ccfa9
msantanavargas <m.santana.sp@mac.com>
2025-02-23 18:09:08 -0800

    added more Vulkan Examples

---
+++ C/Vulcan/Triangle_Nvidia/cmake/CMakeFiles/my_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_app.dir/link.d"
+  "CMakeFiles/my_app.dir/src/main.cpp.o"
+  "CMakeFiles/my_app.dir/src/main.cpp.o.d"
+  "my_app"
+  "my_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
