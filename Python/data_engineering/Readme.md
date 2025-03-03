28588a0373f18c819d38de72f051a240f0698ed8
Mauricio <m.santana.sp@mac.com>
2025-03-02 19:55:49 -0800

    added Python logic

--- Python/data_engineering/Readme.md
+++ Python/data_engineering/Readme.md
@@ -4,5 +4,6 @@ In this example, data is provided as a raw [SQL data table schema file](https://
 
 The logic goes as follows:
 
-1. Import - Transform from SQL to pandas via [bash](https://github.com/msantanavargas/Coding-Examples/blob/main/Python/data_engineering/text_preprocessing_from_sql_to_pd.sh) generating a [raw text file](https://github.com/msantanavargas/Coding-Examples/blob/main/Python/data_engineering/my_raw_data.txt)
-2. Process in Pandas - The tgext file can then be imported into dataframe and calculate rolling 3-day average via window function in a [python notebook](https://github.com/msantanavargas/Coding-Examples/blob/main/Python/data_engineering/3-day-rolling_average.ipynb).
+1. Import - Transform from SQL code file to a pandas usable text file via [bash](https://github.com/msantanavargas/Coding-Examples/blob/main/Python/data_engineering/text_preprocessing_from_sql_to_pd.sh) generating a [raw text file](https://github.com/msantanavargas/Coding-Examples/blob/main/Python/data_engineering/my_raw_data.txt)
+2. Process in Pandas - The text file can then be imported into dataframe and calculate rolling 3-day average via window function in a [python notebook](https://github.com/msantanavargas/Coding-Examples/blob/main/Python/data_engineering/3-day-rolling_average.ipynb). This is normally done as one-time analysis item.
+3. Put this in production for a regular (daily) run.
---
+++ Python/data_engineering/analytics.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+#########################
+# Importing Data        #
+#########################
+my_file_name = 'my_raw_data.txt'
+print('### IMPORTING DATA ###')
+try:
+    my_data = pd.read_csv(my_file_name, sep=",", header=None, usecols=[0,1]) # Dropping white spaces at the end (third column)
+    my_data.columns = ["transaction_time", "transaction_amount"]
+    my_data.transaction_time = pd.to_datetime(my_data.transaction_time)  # convert to Date/Time
+    my_data['transaction_amount'] = my_data['transaction_amount'].astype(float) # convert to float
+    print(f'### Imported file: {my_file_name} ###')
+    print(f'### Imported {my_data.shape[0]} rows ###')
+except:
+    print(f'### ERROR Importing Data file: {my_file_name} ')
+
+#########################
+# Data Quality Checks   #
+#########################
+
+
+#########################
+# Calculating Metrics   #
+#########################
+# Group by Date and Sum transactions per date
+print('### Calculating Transactions per Day ###')
+my_summary_date = my_data['transaction_amount'].groupby([my_data['transaction_time'].dt.date]).sum() # Get stotals per day
+my_summary_date.index = pd.to_datetime(my_summary_date.index) # transform grouped dates to date types
+
+#Calculate 3-day window averages per day
+print('### Calculating 3-Day Window Averages ###')
+my_rolling_averages = my_summary_date.rolling(window=3).mean() # Do 3 day rolling window mean
+my_rolling_averages.index = pd.to_datetime(my_rolling_averages.index) # transform grouped dates to date types
+
+#########################
+# Exporting Output file #
+#########################
+my_output_file_name = 'my_output_data.txt'
+try:
+    print('### Exporting file ###')
+    my_rolling_averages.to_csv(my_output_file_name, index=True)
+    print(f'### Exported file: {my_output_file_name} ###')
+except:
+    print(f'### ERROR Exporting Data file: {my_output_file_name} ')
